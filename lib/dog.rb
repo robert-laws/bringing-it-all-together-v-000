7f0e93069620e84b2c442cb57930e515995deef1
Robert Laws <roblaws@me.com>
2017-10-14 16:04:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,4 +23,24 @@ class Dog
     sql = "DROP TABLE Dogs"
     DB[:conn].execute(sql)
   end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed) 
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 end
