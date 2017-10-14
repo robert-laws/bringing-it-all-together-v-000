6cac9a09a965d97e2d048ed64cc13455c874d9f6
Robert Laws <roblaws@me.com>
2017-10-14 15:49:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,5 +7,8 @@ class Dog
     @breed = breed
   end
 
-
+  def self.create_table
+    sql = "CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)"
+    DB[:conn].execute(sql)
+  end
 end
